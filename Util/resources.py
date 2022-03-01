d33080bb5797b2406d48303485aca53504abcfee
hsmgr12 <55281798+hsmgr12@users.noreply.github.com>
2022-03-02 00:04:34 +1100

    Util/resources.py

---
+++ Util/resources.py
@@ -0,0 +1,6 @@
+"""
+Setting REST API endpoint and headers.
+"""
+class resources:
+    endpoint = "https://eacp.energyaustralia.com.au/codingtest/api/v1/festivals"
+    headers = {'Content-Type': 'application/json'}
